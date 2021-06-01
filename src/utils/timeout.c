cebff244bd1bbc66275282481b5de8d67cca6918
rpbritton <git@rbritton.dev>
2021-06-01 10:44:39 -0400

    Fix a memory leak

--- src/utils/timeout.c
+++ src/utils/timeout.c
@@ -1,18 +1,18 @@
 /**
  * Copyright (C) 2021 Ryan Britton
- * 
+ *
  * This file is part of Goodnight Mouse.
- * 
+ *
  * Goodnight Mouse is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Goodnight Mouse is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Goodnight Mouse.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -50,6 +50,9 @@ void timeout_enable()
 
     // make a call that will actually force the timeout setting
     atspi_accessible_get_id(desktop, NULL);
+
+    // unref the accessible
+    g_object_unref(desktop);
 }
 
 void timeout_disable()
@@ -68,6 +71,9 @@ void timeout_disable()
 
     // make a call that will actually force the timeout setting
     atspi_accessible_get_id(desktop, NULL);
+
+    // unref the accessible
+    g_object_unref(desktop);
 }
 
 void log_handler(const gchar *log_domain, GLogLevelFlags log_level, const gchar *message, gpointer user_data)
