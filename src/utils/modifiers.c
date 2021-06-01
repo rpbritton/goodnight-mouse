440f78a1e4bed8153a78207b66ddacd26c18fbb2
rpbritton <git@rbritton.dev>
2021-06-01 10:48:09 -0400

    Add modifier helping utility

---
+++ src/utils/modifiers.c
@@ -0,0 +1,32 @@
+/**
+ * Copyright (C) 2021 Ryan Britton
+ *
+ * This file is part of Goodnight Mouse.
+ *
+ * Goodnight Mouse is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Goodnight Mouse is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Goodnight Mouse.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "modifiers.h"
+
+GdkKeymap *default_keymap = NULL;
+
+GdkModifierType map_modifiers(GdkModifierType modifiers)
+{
+    if (!default_keymap)
+        default_keymap = gdk_keymap_get_for_display(gdk_display_get_default());
+
+    gdk_keymap_map_virtual_modifiers(default_keymap, &modifiers);
+
+    return modifiers & 0xFF;
+}
